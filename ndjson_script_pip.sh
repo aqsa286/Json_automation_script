66e71821a8acda8bf9f1073f12b2b02e4795a23e
aqsa286 <aqsafarooq402@gmail.com>
2024-02-19 17:27:13 +0500

    Updated domain file

---
+++ ndjson_script_pip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+filename=$1
+product=$2
+endpoint=$3
+request_uri=$4
+index=$5
+
+mapfile -t client_urls < domain_file
+
+for ((i=1; i<${#client_urls[@]}; i++)); do
+    if grep -q "DOMAIN$i\b" "$filename"; then
+        sed -i "s/DOMAIN$i\b/${client_urls[i]}/g" "$filename"
+    else
+        echo "Error: 'DOMAIN$i' not found in $filename."
+    fi
+done
+cat domain_file
+
+sed -i "s/PRODUCT/$product/g" "$filename"
+sed -i "s/ENDPOINT/$endpoint/g" "$filename"
+sed -i "s/REQ_URI$request_uri/g" "$filename"
+sed -i "s/INDEX/$index/g" "$filename"
+
+echo "Replacements complete in $filename."
